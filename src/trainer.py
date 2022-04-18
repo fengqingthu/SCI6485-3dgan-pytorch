c0769fde0d9c521382ae3c51670831d6e607b04e
fengqing <fengqingthu@gmail.com>
2022-04-17 22:34:16 -0400

    Fix pretrain feature

--- src/trainer.py
+++ src/trainer.py
@@ -88,7 +88,7 @@ def trainer(args):
 
     # load pretrained model
     if params.pretrained_dir and os.path.exists(params.pretrained_dir+"G.pth"):
-        G.load_state_dict(torch.load(params.pretrained_dir + "G.pth"))
+        G = torch.load(params.pretrained_dir + "G.pth")
 
     D_solver = optim.Adam(D.parameters(), lr=params.d_lr, betas=params.beta)
     # D_solver = optim.SGD(D.parameters(), lr=args.d_lr, momentum=0.9)
